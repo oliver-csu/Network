66ae4c21a2ace2f8e08aa342c990c91c3d1c395f
oliver-csu <oliver.csu@gmail.com>
2021-03-01 17:40:38 +0800

    原生JDK网络编程BIO-服务端

--- network-jdk/src/main/java/ren/oliver/network/jdk/bio/server/BioServer.java
+++ network-jdk/src/main/java/ren/oliver/network/jdk/bio/server/BioServer.java
@@ -1,11 +1,39 @@
 package ren.oliver.network.jdk.bio.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static ren.oliver.network.jdk.bio.Constant.DEFAULT_PORT;
+
 // 服务端
 public class BioServer {
 
-    // 程序入口
-    public static void main(String[] args) {
+    // 服务器端
+    private static ServerSocket server;
 
+    // 线程池，处理每个客户端的请求
+    private static ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+    // 程序入口
+    public static void main(String[] args) throws IOException {
+        try {
+            // 通过构造函数创建ServerSocket，如果端口合法且空闲，服务端就监听成功
+            server = new ServerSocket(DEFAULT_PORT);
+            System.out.println("服务器已启动，端口号：" + DEFAULT_PORT);
+            while(true) {
+                Socket socket= server.accept();
+                System.out.println("有新的客户端连接：" + socket.getPort());
+                // 当有新的客户端接入时，打包成一个任务，投入线程池
+                executorService.execute(new BioServerHandler(socket));
+            }
+        } finally {
+            if (server != null) {
+                server.close();
+            }
+        }
     }
 
 
